7a359ba80358a6627d31fd4a6dbcfc51956e1512
kihyun <mable0927@gmail.com>
2024-03-26 14:18:01 +0900

    과제 완료

---
+++ 0325_PythonBasic/실습3.py
@@ -0,0 +1,6 @@
+introduce = ["kihyun", "INFJ", "24"]
+
+print("안녕하세요 저는 멋쟁이 사자처럼 백엔드파트이고")
+print("이름은 " + introduce[0] + "입니다")
+print("MBTI는 " + introduce[1] + "이고")
+print("나이는 " + introduce[2] + "살 입니다.")
---
+++ 0325_PythonBasic/실습4.py
@@ -0,0 +1,3 @@
+OurTeam={"김민경":[24,"컴공", "ESFJ"],"손가영" : [21,"컴공", "INFP"],"심성민":[26,"일본어문학과","INFJ"]}
+for name in OurTeam:
+    print(name,OurTeam[name][0],OurTeam[name][1],OurTeam[name][2])
