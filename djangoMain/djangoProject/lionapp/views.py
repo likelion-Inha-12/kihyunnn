9edaf2e710de39be33d733607a24e952856cffee
kihyun <mable0927@gmail.com>
2024-05-07 12:40:31 +0900

    drf개인과제 완료

--- djangoMain/djangoProject/lionapp/views.py
+++ djangoMain/djangoProject/lionapp/views.py
@@ -12,23 +12,6 @@ from rest_framework import status
 from rest_framework.views import APIView
 
 
-# def create_post(request):
-#     if request.method == 'POST':
-#         data = json.loads(request.body)
-
-#         title = data.get('title')
-#         content = data.get('content')
-
-#         post = Post(
-#             title = title,
-#             content = content
-#         )
-#         post.save()
-#         return JsonResponse({'message':'success'})
-#     return JsonResponse({'message':'POST 요청만 허용됩니다.'})
-
-
-
 def api_response(data=None, message="", status=200):
     response = {
         "message": message,
@@ -67,28 +50,6 @@ class PostApiView(APIView): #APIView를 이용한 get, delete post
         
         message = f"id: {pk}번 포스트 삭제 성공"
         return api_response(message = message, status = status.HTTP_200_OK)
-
-# def get_post(request, pk): 리팩토링 이전 코드들
-#     post = get_object_or_404(Post, pk=pk)
-#     data = {
-#         'id': post.pk,
-#         '제목' : post.title,
-#         '내용' : post.content,
-#         '메시지' : '조회 성공'
-#     }
-#     return JsonResponse(data, status=200)
-
-
-# def delete_post(request, pk): 리팩토링 이전 코드들
-#     if request.method == 'DELETE':
-#         post = get_object_or_404(Post, pk=pk)
-#         post.delete()
-#         data = {
-#             "message" : f"id: {pk} 포스트 삭제 완료"
-#         }
-#         return JsonResponse(data, status=200)
-#     return JsonResponse({'message':'DELETE 요청만 허용됩니다.'})
-
 
 from django.http import HttpResponse
 
